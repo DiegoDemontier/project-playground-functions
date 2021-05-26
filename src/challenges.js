ac72faac1f6a65f0c075e3376d3f3c24fad4532f
Demontier <demontier@outlook.com>
2021-05-26 00:23:38 -0300

    requisitos feitos: 1, 2, 3, 4, 5, 6, 7.

--- src/challenges.js
+++ src/challenges.js
@@ -1,36 +1,71 @@
 // Desafio 1
-function compareTrue() {
-  
+function compareTrue(compare1, compare2) {
+  let comparacao = Boolean;
+  if (compare1 === true && compare2 === true) {
+    comparacao = true;
+  } else {
+    comparacao = false;
+  }
+  return comparacao;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(palavras) {
+  let arrayPalavras = palavras.split(' ');
+  return arrayPalavras;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayString) {
+  let palavra = `${arrayString[arrayString.length - 1]} ${arrayString[0]}`;
+  return palavra;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = (wins * 3 + ties * 1);
+  return pontos;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numeros) {
+  let maiorNumero = numeros[0];
+  let quantidadeVezes = 0;
+
+  for (let i = 0; i < numeros.length; i += 1) {
+    if (numeros[i] > maiorNumero) {
+      maiorNumero = numeros[i];
+      quantidadeVezes = 1;
+    } else if (numeros[i] === maiorNumero) {
+      quantidadeVezes += 1;
+    }
+  }
+  return quantidadeVezes;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let dist1 = 0;
+  let dist2 = 0;
+  let pegaRato = '';
+
+  dist1 = cat1 - mouse;
+  dist2 = cat2 - mouse;
+
+  if (Math.abs(dist1) === Math.abs(dist2)) {
+    pegaRato = 'os gatos trombam e o rato foge';
+  } else if (Math.abs(dist1) > Math.abs(dist2)) {
+    pegaRato = 'cat2';
+  } else {
+    pegaRato = 'cat1';
+  }
+
+  return pegaRato;
 }
 
 // Desafio 8
